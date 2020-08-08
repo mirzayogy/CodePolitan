3811fdd06eaf936f4f43f69faedead52f54e8a45
mirzayogy <mirza.yogy@gmail.com>
2020-08-09 01:26:59 +0800

    menggunakan method index pada homecontroller

--- Laravel/perpustakaan/routes/admin.php
+++ Laravel/perpustakaan/routes/admin.php
@@ -2,6 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route ::get('/', function () {
-    return "admin page";
-})->name('dashboard');
+Route ::get('/', 'HomeController@index')->name('dashboard');
