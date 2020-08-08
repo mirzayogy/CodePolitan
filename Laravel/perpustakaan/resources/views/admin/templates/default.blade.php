c62cd0e1e2f67e2361039d0ccd50ae2bfc6b1289
mirzayogy <mirza.yogy@gmail.com>
2020-08-09 01:48:51 +0800

    buang navbar yang tidak terpakai

--- Laravel/perpustakaan/resources/views/admin/templates/default.blade.php
+++ Laravel/perpustakaan/resources/views/admin/templates/default.blade.php
@@ -43,11 +43,6 @@
         </div>
         <!-- /.content-wrapper -->
         @include('admin.templates.partials.footer')
-        <!-- Control Sidebar -->
-        <aside class="control-sidebar control-sidebar-dark">
-            <!-- Control sidebar content goes here -->
-        </aside>
-        <!-- /.control-sidebar -->
     </div>
     <!-- ./wrapper -->
 
