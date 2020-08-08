8b4b6060dd8e0364522cb03a348d7904fcb90466
mirzayogy <mirza.yogy@gmail.com>
2020-08-08 20:51:33 +0800

    tes route

--- Laravel/perpustakaan/routes/web.php
+++ Laravel/perpustakaan/routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('admin.templates.default');
 });
+
+Route::get('/users', function () {
+    return view('admin.users.index');
+});
