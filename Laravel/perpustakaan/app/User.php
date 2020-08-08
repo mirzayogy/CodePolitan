7d65df894342840537a74b066f8f9923981fda40
mirzayogy <mirza.yogy@gmail.com>
2020-08-08 22:40:35 +0800

    implementasi verify email & filter user hanya yang verified

--- Laravel/perpustakaan/app/User.php
+++ Laravel/perpustakaan/app/User.php
@@ -2,12 +2,11 @@
 
 namespace App;
 
-//migrate dulu jangan lupa
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use Notifiable;
 
--- Laravel/perpustakaan/routes/web.php
+++ Laravel/perpustakaan/routes/web.php
@@ -21,6 +21,6 @@ Route::get('/users', function () {
     return view('admin.users.index');
 });
 
-Auth::routes();
+Auth::routes(['verify' => true]);
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home')->middleware('verified');
