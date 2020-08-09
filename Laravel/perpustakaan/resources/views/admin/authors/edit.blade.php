9313bb61e7f03b833a7327323d0fcf932a7de8c4
mirzayogy <mirza.yogy@gmail.com>
2020-08-09 13:42:52 +0800

    EDIT - duplicate CREATE

---
+++ Laravel/perpustakaan/resources/views/admin/authors/edit.blade.php
@@ -0,0 +1,23 @@
+@extends('admin.templates.default')
+
+@section('content')
+    <h1>Penulis</h1>
+
+    <div class="card">
+        <div class="card-header">
+            <h3 class="card-title">Tambah Data Penulis</h3>
+        </div>
+        <div class="card-body">
+        <form action="{{route('admin.author.store')}}" method="POST">
+                @csrf
+                <div class="form-group">
+                    <label for="">Nama</label>
+                    <input type="text" name="name" class="form-control" placeholder="Masukkan nama penulis">
+                </div>
+                <div class="form-group">
+                    <button type="submit" value="Tambah" class="btn btn-success"><i class="fa fa-save"></i>  Simpan</button>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
