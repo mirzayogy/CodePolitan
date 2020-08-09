728d201742ef1bb1a11606807882913241fe460f
mirzayogy <mirza.yogy@gmail.com>
2020-08-09 10:09:46 +0800

    buat table.. kayanya masih error

--- Laravel/perpustakaan/resources/views/admin/authors/index.blade.php
+++ Laravel/perpustakaan/resources/views/admin/authors/index.blade.php
@@ -2,4 +2,21 @@
 
 @section('content')
     <h1>Penulis</h1>
+
+    <div class="box">
+        <table id="dataTable" class="table table-bordered table-striped dataTable">
+            <thead>
+                <tr>
+                    <td>Id</td>
+                    <td>Nama</td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Satu</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 @endsection
