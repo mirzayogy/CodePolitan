3efd5365be1b56e6b4a1c9edd71dd1ae16fe6e50
mirzayogy <mirza.yogy@gmail.com>
2020-08-09 08:30:21 +0800

    set isi data buku, dengan cover, dan foreign key

--- Laravel/perpustakaan/database/factories/BookFactory.php
+++ Laravel/perpustakaan/database/factories/BookFactory.php
@@ -2,11 +2,22 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
+use App\Author;
 use App\Book;
 use Faker\Generator as Faker;
 
 $factory->define(Book::class, function (Faker $faker) {
-    return [
 
+    $randomNumber = rand(1,100);
+    $cover = "https://picum.photos/id/{$randomNumber}/200,300";
+
+    return [
+        'author_id' => Author::inRandomOrder()->first()->id,
+        'title' => $faker->sentence(4),
+        'description' => $faker->sentence(50),
+        'cover' => $cover,
+        'qty' => rand(10,20),
     ];
+
+
 });
