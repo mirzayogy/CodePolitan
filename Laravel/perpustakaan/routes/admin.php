57cea6c8311e78cf6a963035f355856ae5fdeaea
mirzayogy <mirza.yogy@gmail.com>
2020-08-09 10:11:09 +0800

    route ke data

--- Laravel/perpustakaan/routes/admin.php
+++ Laravel/perpustakaan/routes/admin.php
@@ -5,3 +5,5 @@ use Illuminate\Support\Facades\Route;
 Route ::get('/', 'HomeController@index')->name('dashboard');
 
 Route ::get('/author', 'AuthorController@index')->name('author.index');
+
+Route ::get('/author/data', 'DataController@authors')->name('author.data');
