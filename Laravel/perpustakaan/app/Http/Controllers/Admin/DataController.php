27182b823df6c2fdd2cb011f09a971c3738a517d
mirzayogy <mirza.yogy@gmail.com>
2020-08-09 10:10:41 +0800

    php artisan make:controller Admin\\DataController

---
+++ Laravel/perpustakaan/app/Http/Controllers/Admin/DataController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Author;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DataController extends Controller
+{
+    //
+}
