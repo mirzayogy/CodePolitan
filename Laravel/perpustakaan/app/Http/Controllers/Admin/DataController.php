8f4735de2efddc35866650498bc1de5e12e38f60
mirzayogy <mirza.yogy@gmail.com>
2020-08-12 10:48:48 +0800

    BUKU - DATA

--- Laravel/perpustakaan/app/Http/Controllers/Admin/DataController.php
+++ Laravel/perpustakaan/app/Http/Controllers/Admin/DataController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Author;
+use App\Book;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
@@ -14,6 +15,16 @@ class DataController extends Controller
 
         return datatables()->of($authors)
             ->addColumn('action', 'admin.authors.action')
+            ->addIndexColumn()
+            ->toJson();
+    }
+
+    public function books(){
+
+        $books = Book::orderBy('title', 'ASC');
+
+        return datatables()->of($books)
+            // ->addColumn('action', 'admin.books.action')
             ->addIndexColumn()
             ->toJson();
     }
--- Laravel/perpustakaan/routes/admin.php
+++ Laravel/perpustakaan/routes/admin.php
@@ -5,5 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route ::get('/', 'HomeController@index')->name('dashboard');
 
 Route ::get('/author/data', 'DataController@authors')->name('author.data');
+Route ::get('/book/data', 'DataController@books')->name('book.data');
 
 Route::resource('author', 'AuthorController');
