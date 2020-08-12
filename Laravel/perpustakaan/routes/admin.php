2fb367b24b215092e67000f6b1e2f97795894d2a
mirzayogy <mirza.yogy@gmail.com>
2020-08-12 10:54:58 +0800

    BUKU - ROUTE

--- Laravel/perpustakaan/routes/admin.php
+++ Laravel/perpustakaan/routes/admin.php
@@ -8,3 +8,4 @@ Route ::get('/author/data', 'DataController@authors')->name('author.data');
 Route ::get('/book/data', 'DataController@books')->name('book.data');
 
 Route::resource('author', 'AuthorController');
+Route::resource('book', 'BookController');
