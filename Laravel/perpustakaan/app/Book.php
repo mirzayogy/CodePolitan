d8566ea46f3f97eca94c980a8ea6bcf070c9655e
mirzayogy <mirza.yogy@gmail.com>
2020-08-15 22:13:39 +0800

    BOOK - CREATE model guarded dan method getCover

--- Laravel/perpustakaan/app/Book.php
+++ Laravel/perpustakaan/app/Book.php
@@ -6,8 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
+
+    protected $guarded = [];
+
     public function author()
     {
         return $this->belongsTo(Author::class);
+    }
+
+    public function getCover(){
+        if(substr($this->cover,0,5) == 'https'){
+            return $this->cover;
+        }
+
+        if($this->cover){
+            return asset($this->cover);
+        }
+
+        return "https://via.placeholder.com/100x150.png?text=no+cover";
     }
 }
