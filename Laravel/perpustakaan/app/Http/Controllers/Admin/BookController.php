d6b8dddb3011a6082f83b0defcccd0bcb9118c6b
mirzayogy <mirza.yogy@gmail.com>
2020-08-15 23:43:16 +0800

    BOOK - EDIT controller + hasil update gambar

--- Laravel/perpustakaan/app/Http/Controllers/Admin/BookController.php
+++ Laravel/perpustakaan/app/Http/Controllers/Admin/BookController.php
@@ -6,6 +6,7 @@ use App\Author;
 use App\Book;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class BookController extends Controller
 {
@@ -84,9 +85,14 @@ class BookController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Book $book)
     {
-        //
+        return view('admin.books.edit',[
+            'title' => 'Ubah Data Buku',
+            'book' => $book,
+            'authors' => Author::orderby('name', 'ASC')->get()
+        ]);
+
     }
 
     /**
@@ -96,9 +102,32 @@ class BookController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Book $book)
     {
-        //
+        $this->validate($request, [
+            'title' => 'required',
+            'description' => 'required|min:20',
+            'author_id' => 'required',
+            'cover' => 'file|image',
+            'qty' => 'required|numeric'
+        ]);
+
+        $cover = $book->cover;
+
+        if ($request->hasFile('cover')) {
+            Storage::delete($book->cover);
+            $cover = $request->file('cover')->store('assets/cover');
+        }
+
+        $book->update([
+            'title' => $request->title,
+            'description' => $request->description,
+            'author_id' => $request->author_id,
+            'cover' => $cover,
+            'qty' => $request->qty,
+        ]);
+
+        return redirect()->route('admin.book.index')->withSuccess('Data Buku berhasil diperbaharui');
     }
 
     /**
