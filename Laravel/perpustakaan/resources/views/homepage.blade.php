952796f8ce69fc2c6909f54a694ed2efca7cc961
mirzayogy <mirza.yogy@gmail.com>
2020-08-16 10:43:46 +0800

    USER - pagination

--- Laravel/perpustakaan/resources/views/homepage.blade.php
+++ Laravel/perpustakaan/resources/views/homepage.blade.php
@@ -51,7 +51,15 @@
             @endfor
         </div>
 
-
+        <ul class="pagination">
+            <li class="disabled"><a href="#!"><i class="material-icons">chevron_left</i></a></li>
+            <li class="active"><a href="#!">1</a></li>
+            <li class="waves-effect"><a href="#!">2</a></li>
+            <li class="waves-effect"><a href="#!">3</a></li>
+            <li class="waves-effect"><a href="#!">4</a></li>
+            <li class="waves-effect"><a href="#!">5</a></li>
+            <li class="waves-effect"><a href="#!"><i class="material-icons">chevron_right</i></a></li>
+        </ul>
     </div>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
