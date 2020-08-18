e68d48f0b7f9783b6cc49f9d469ffbd6ccc37de5
mirzayogy <mirza.yogy@gmail.com>
2020-08-18 14:38:54 +0800

    USER - buku lainnya

--- Laravel/perpustakaan/resources/views/frontend/book/show.blade.php
+++ Laravel/perpustakaan/resources/views/frontend/book/show.blade.php
@@ -9,9 +9,7 @@
         <div class="row">
                 <div class="col s12 m12">
                     <div class="card horizontal hoverable">
-                        <div class="card-image">
-                            <img src="{{ $book->getCOver() }}">
-                        </div>
+                        <img src="{{ $book->getCOver() }}">
                         <div class="card-stacked">
                             <div class="card-content">
                             <h4 class="red-text accent-2">{{$book->title}}</h4>
@@ -32,7 +30,31 @@
                     </div>
                 </div>
         </div>
+    </div>
 
-        {{-- {{$books->render('vendor.pagination.materialize')}} --}}
+    <div class="container">
+        <h5>Buku lainnya dari penulis {{$book->author->name}}</h5>
+        <div class="row">
+            @foreach ($book->author->books->shuffle()->take(4) as $book)
+                <div class="col s12 m6">
+                    <div class="card horizontal hoverable">
+                        <div class="card-image">
+                            <img src="{{ $book->getCOver() }}" height="200px">
+                        </div>
+                        <div class="card-stacked">
+                            <div class="card-content">
+                            <h6>
+                            <a href="{{route('book.show',$book)}}">{{ Str::limit($book->title,20)}}</a>
+                            </h6>
+                            <p>{{ Str::limit($book->description,30)}}</p>
+                            </div>
+                            <div class="card-action">
+                            <a href="#">Pinjam Buku</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
     </div>
 @endsection
