8b53069fda76b4ed330afa4de8a3f4166a93986c
mirzayogy <mirza.yogy@gmail.com>
2020-08-18 15:19:46 +0800

    php artisan make:controller BorrowHistoryController

---
+++ Laravel/perpustakaan/app/Http/Controllers/BookHistoryController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BookHistoryController extends Controller
+{
+    private $table = 'borrow_history';
+
+    private $guarded = [];
+}
