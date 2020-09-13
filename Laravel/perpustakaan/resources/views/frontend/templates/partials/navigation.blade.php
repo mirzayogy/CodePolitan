ab4471809dfb06db94323f42b122e408913f0407
amukancabbage <amukancabbage@gmail.com>
2020-09-13 09:16:42 +0800

    guest

--- Laravel/perpustakaan/resources/views/frontend/templates/partials/navigation.blade.php
+++ Laravel/perpustakaan/resources/views/frontend/templates/partials/navigation.blade.php
@@ -4,8 +4,21 @@
             <a href="{{ route('homepage') }}" class="brand-logo">Perpusku</a>
             <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
             <ul class="right hide-on-med-and-down">
-                <li><a href="{{ route('login') }}">Login</a></li>
-                <li><a href="{{ route('register') }}">Register</a></li>
+                @guest
+                    <li><a href="{{ route('login') }}">Login</a></li>
+                    <li><a href="{{ route('register') }}">Register</a></li>
+                @else
+                    <ul id="dropdown1" class="dropdown-content">
+                        <li>
+                            <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                            Logout
+                            </a>
+                        </li>
+                    </ul>
+                    <form id="logout-form" action="{{ route('logout')}}" method="POST" style="display:none">@csrf</form>
+                    <li><a href="#" class="dropdown-trigger" data-target="dropdown1">{{auth()->user()->name}}</a></li>
+
+                @endguest
             </ul>
         </div>
     </nav>
@@ -13,6 +26,8 @@
 
 
 <ul class="sidenav" id="mobile-demo">
-    <li><a href="{{ route('login') }}">Login</a></li>
-    <li><a href="{{ route('register') }}">Register</a></li>
+    @guest
+        <li><a href="{{ route('login') }}">Login</a></li>
+        <li><a href="{{ route('register') }}">Register</a></li>
+    @endguest
 </ul>
